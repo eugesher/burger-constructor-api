786ac51df8c74bcf4604ef866d4330f668e13911
Evgenii Chernyshov <mailbox@eugesher.site>
2021-06-28 17:05:25 +0300

    feat: user model

---
+++ models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: validator.isEmail,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
