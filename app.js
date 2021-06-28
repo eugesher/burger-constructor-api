183ea1fa47549550d5b565d004c784c0dbd4e679
Evgenii Chernyshov <mailbox@eugesher.site>
2021-06-28 18:18:49 +0300

    feat: test user

--- app.js
+++ app.js
@@ -18,6 +18,14 @@ mongoose.connect('mongodb://localhost:27017/burgersdb', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: '60d9e793860bfa3caa38d71f',
+  };
+
+  next();
+});
+
 app.use(router);
 
 app.listen(PORT);
