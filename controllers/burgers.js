571a9136f8d80f91d295b80375e3776f5d455244
Evgenii Chernyshov <mailbox@eugesher.site>
2021-07-07 17:11:35 +0300

    feat: calculate burger price

--- controllers/burgers.js
+++ controllers/burgers.js
@@ -3,7 +3,7 @@ const Ingredient = require('../models/ingredient');
 const BadRequestError = require('../erorrs/bad-request-error');
 const ForbiddenError = require('../erorrs/forbidden-error');
 const NotFoundError = require('../erorrs/not-found-error');
-const { concatenateErrorMessages } = require('../utils');
+const { concatenateErrorMessages, validateBurgerComposition } = require('../utils');
 
 module.exports.getBurgers = (req, res, next) => {
   const owner = req.user._id;
@@ -18,42 +18,16 @@ module.exports.saveBurger = (req, res, next) => {
   const { name, ingredients } = req.body;
   const owner = req.user._id;
 
-  function validateBurgerComposition() {
-    const bunsLength = ingredients.filter((i) => i.category === 'buns').length;
-    const cutletsLength = ingredients.filter((i) => i.category === 'cutlets').length;
-    const cheesesLength = ingredients.filter((i) => i.category === 'cheeses').length;
-    const vegetablesLength = ingredients.filter((i) => i.category === 'vegetables').length;
-    const saucesLength = ingredients.filter((i) => i.category === 'sauces').length;
-
-    if (!bunsLength) {
-      throw new BadRequestError('burger must contain a bun');
-    }
-    if (bunsLength > 1) {
-      throw new BadRequestError('there should only be one bun in a burger');
-    }
-    if (cutletsLength > 2) {
-      throw new BadRequestError('burger should not contain more than 2 cutlets');
-    }
-    if (cheesesLength > 2) {
-      throw new BadRequestError('burger should not contain more than 2 servings of cheese');
-    }
-    if (vegetablesLength > 3) {
-      throw new BadRequestError('burger should not contain more than 3 servings of vegetable');
-    }
-    if (saucesLength > 3) {
-      throw new BadRequestError('burger should not contain more than 3 servings of sauce');
-    }
-  }
-
   function calculateBurgerPrice() {
-    return Ingredient.find({ _id: { $in: ingredients } })
-      .then((foundIngredients) => {
-        if (foundIngredients.length !== ingredients.length) {
-          throw new BadRequestError('some ingredients not found');
-        }
-        validateBurgerComposition();
-        return foundIngredients.reduce((sum, current) => sum + current.price, 0);
-      });
+    return Ingredient.find({ _id: { $in: ingredients } }).then((foundIngredients) => {
+      const burgerIngredients = ingredients.map((i) => foundIngredients.find((fi) => {
+        const match = String(fi._id) === i;
+        if (!match) throw new BadRequestError('some ingredients not found');
+        return match;
+      }));
+      validateBurgerComposition(burgerIngredients);
+      return burgerIngredients.reduce((sum, current) => sum + current.price, 0);
+    });
   }
 
   calculateBurgerPrice()
@@ -69,7 +43,7 @@ module.exports.saveBurger = (req, res, next) => {
     })
     .catch((err) => {
       if (err.name === 'ValidationError') next(new BadRequestError(concatenateErrorMessages(err)));
-      else if (err.kind === 'ObjectId') next(new BadRequestError('invalid ingredients id'));
+      else if (err.kind === 'ObjectId') next(new BadRequestError('invalid ingredient ids'));
       else next(err);
     });
 };
@@ -82,8 +56,7 @@ module.exports.deleteBurger = (req, res, next) => {
     .then((burger) => {
       if (burger) {
         if (burger.owner.equals(userId)) {
-          Burger.findByIdAndRemove(burgerId)
-            .then((removedBurger) => res.send(removedBurger));
+          Burger.findByIdAndRemove(burgerId).then((removedBurger) => res.send(removedBurger));
         } else {
           throw new ForbiddenError('only your burgers are allowed to delete');
         }
@@ -91,9 +64,5 @@ module.exports.deleteBurger = (req, res, next) => {
         throw new NotFoundError('burger not found');
       }
     })
-    .catch((err) => next(
-      err.kind === 'ObjectId'
-        ? new BadRequestError('invalid burger id')
-        : err,
-    ));
+    .catch((err) => next(err.kind === 'ObjectId' ? new BadRequestError('invalid burger id') : err));
 };
--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,13 @@
+const BadRequestError = require('../erorrs/bad-request-error');
+
 const schemaOptions = { toJSON: { getters: true }, id: false };
+const burgerIngredientLimits = {
+  buns: 1,
+  cutlets: 2,
+  cheeses: 2,
+  vegetables: 3,
+  sauces: 3,
+};
 
 const temporaryUserHandler = (req, res, next) => {
   req.user = {
@@ -23,11 +32,46 @@ function concatenateErrorMessages(err) {
   return Object.values(err.errors).map((e) => e.message).join('. ');
 }
 
+function validateBurgerComposition(burgerIngredients) {
+  const bunsCorrect = burgerIngredients.filter((i) => i.category === 'buns')
+    .length === burgerIngredientLimits.buns;
+  const cutletsCorrect = burgerIngredients.filter((i) => i.category === 'cutlets')
+    .length <= burgerIngredientLimits.cutlets;
+  const cheesesCorrect = burgerIngredients.filter((i) => i.category === 'cheeses')
+    .length <= burgerIngredientLimits.cheeses;
+  const vegetablesCorrect = burgerIngredients.filter((i) => i.category === 'vegetables')
+    .length <= burgerIngredientLimits.vegetables;
+  const saucesCorrect = burgerIngredients.filter((i) => i.category === 'sauces')
+    .length <= burgerIngredientLimits.sauces;
+
+  if (
+    bunsCorrect && cutletsCorrect && cheesesCorrect && vegetablesCorrect && saucesCorrect
+  ) return;
+
+  if (!bunsCorrect) {
+    throw new BadRequestError('burger must contain 1 bun');
+  }
+  if (!cutletsCorrect) {
+    throw new BadRequestError('burger should not contain more than 2 cutlets');
+  }
+  if (!cheesesCorrect) {
+    throw new BadRequestError('burger should not contain more than 2 servings of cheese');
+  }
+  if (!vegetablesCorrect) {
+    throw new BadRequestError('burger should not contain more than 3 servings of vegetable');
+  }
+  if (!saucesCorrect) {
+    throw new BadRequestError('burger should not contain more than 3 servings of sauce');
+  }
+}
+
 module.exports = {
   schemaOptions,
+  burgerIngredientLimits,
   temporaryUserHandler,
   getPrice,
   setPrice,
   setName,
   concatenateErrorMessages,
+  validateBurgerComposition,
 };
