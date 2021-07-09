6286b65da97024541f9182af6baeed10069e1836
Evgenii Chernyshov <mailbox@eugesher.site>
2021-07-09 17:52:23 +0300

    feat: update ingredients quantity on making order

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,5 +1,6 @@
 const Order = require('../models/order');
 const Burger = require('../models/burger');
+const Ingredient = require('../models/ingredient');
 const BadRequestError = require('../erorrs/bad-request-error');
 const { concatenateErrorMessages } = require('../utils');
 
@@ -26,9 +27,47 @@ module.exports.makeOrder = (req, res, next) => {
     });
   }
 
+  function getIngredients(order) {
+    return Burger.find({ _id: { $in: order.list } })
+      .populate('ingredients')
+      .then((foundBurgers) => {
+        let ingredients = [];
+        foundBurgers.forEach((fb) => {
+          ingredients = [...ingredients, ...fb.ingredients];
+        });
+        return ingredients;
+      });
+  }
+
+  function updateIngredientsQuantity(order) {
+    getIngredients(order)
+      .then((ingredients) => {
+        const ingredientsUsed = [...new Set(ingredients)].map((ingredient) => {
+          let count = 0;
+          for (let i = 0; i < ingredients.length; i += 1) {
+            if (ingredients[i]._id === ingredient._id) count += 1;
+          }
+          return { id: ingredient._id, quantity: ingredient.quantity, count };
+        });
+        const promises = [];
+        ingredientsUsed.forEach((ingredient) => {
+          promises.push(
+            Ingredient.findByIdAndUpdate(
+              ingredient.id, { quantity: ingredient.quantity - ingredient.count }, { new: true },
+            ),
+          );
+        });
+        Promise.all(promises);
+      });
+  }
+
   calculateOrderPrice()
     .then((price) => {
-      Order.create({ list, price, owner }).then((burger) => res.json(burger));
+      Order.create({ list, price, owner })
+        .then((order) => {
+          updateIngredientsQuantity(order);
+          return res.json(order);
+        });
     })
     .catch((err) => {
       if (err.name === 'ValidationError') next(new BadRequestError(concatenateErrorMessages(err)));
