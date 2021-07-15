012465ee7fa479ec93160f3f08b3569c785d64d1
Evgenii Chernyshov <mailbox@eugesher.site>
2021-07-15 17:26:06 +0300

    fix: make order action

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,7 +1,5 @@
 const Order = require('../models/order');
-const BadRequestError = require('../erorrs/bad-request-error');
 const OrderService = require('../services/OrderService');
-const { concatenateErrorMessages } = require('../utils');
 
 module.exports.getOrders = (req, res, next) => {
   const owner = req.user._id;
@@ -16,17 +14,10 @@ module.exports.makeOrder = (req, res, next) => {
   const { list } = req.body;
   const owner = req.user._id;
 
-  OrderService.calculatePrice(list)
-    .then((price) => {
-      Order.create({ list, price, owner })
-        .then((order) => {
-          OrderService.updateStock(order);
-          return res.json(order);
-        });
-    })
-    .catch((err) => {
-      if (err.name === 'ValidationError') next(new BadRequestError(concatenateErrorMessages(err)));
-      else if (err.kind === 'ObjectId') next(new BadRequestError('invalid burger ids'));
-      else next(err);
-    });
+  OrderService.getIngredients(list)
+    .then(OrderService.updateStock)
+    .then(() => OrderService.calculatePrice(list))
+    .then((price) => Order.create({ list, price, owner }))
+    .then((order) => res.json(order))
+    .catch(next);
 };
--- services/OrderService.js
+++ services/OrderService.js
@@ -2,19 +2,19 @@ const Burger = require('../models/burger');
 const Ingredient = require('../models/ingredient');
 const BadRequestError = require('../erorrs/bad-request-error');
 
-function getIngredients(order) {
-  return Burger.find({ _id: { $in: order.list } })
-    .populate('ingredients')
-    .then((foundBurgers) => {
-      let ingredients = [];
-      foundBurgers.forEach((fb) => {
-        ingredients = [...ingredients, ...fb.ingredients];
-      });
-      return ingredients;
-    });
-}
-
 class OrderService {
+  static getIngredients(list) {
+    return Burger.find({ _id: { $in: list } })
+      .populate('ingredients')
+      .then((foundBurgers) => {
+        let ingredients = [];
+        foundBurgers.forEach((fb) => {
+          ingredients = [...ingredients, ...fb.ingredients];
+        });
+        return ingredients;
+      });
+  }
+
   static calculatePrice(list) {
     return Burger.find({ _id: { $in: list } })
       .then((foundBurgers) => {
@@ -26,27 +26,24 @@ class OrderService {
       });
   }
 
-  static updateStock(order) {
-    getIngredients(order)
-      .then((ingredients) => {
-        const ingredientsUsed = [...new Set(ingredients)].map((ingredient) => {
-          let amount = 0;
-          for (let i = 0; i < ingredients.length; i += 1) {
-            if (ingredients[i]._id === ingredient._id) amount += 1;
-          }
-          return { id: ingredient._id, quantity: ingredient.quantity, amount };
-        });
-        const promises = [];
-        ingredientsUsed.forEach((ingredient) => {
-          const quantity = ingredient.quantity - ingredient.amount;
-          if (quantity >= 0) {
-            promises.push(Ingredient.findByIdAndUpdate(ingredient.id, { quantity }));
-          } else {
-            throw new BadRequestError('not enough ingredients in stock');
-          }
-        });
-        Promise.all(promises);
-      });
+  static updateStock(ingredients) {
+    const ingredientsUsed = [...new Set(ingredients)].map((ingredient) => {
+      let amount = 0;
+      for (let i = 0; i < ingredients.length; i += 1) {
+        if (ingredients[i]._id === ingredient._id) amount += 1;
+      }
+      return { id: ingredient._id, quantity: ingredient.quantity, amount };
+    });
+    const promises = [];
+    ingredientsUsed.forEach((ingredient) => {
+      const quantity = ingredient.quantity - ingredient.amount;
+      if (quantity >= 0) {
+        promises.push(Ingredient.findByIdAndUpdate(ingredient.id, { quantity }));
+      } else {
+        throw new BadRequestError('not enough ingredients in stock');
+      }
+    });
+    Promise.all(promises);
   }
 }
 
