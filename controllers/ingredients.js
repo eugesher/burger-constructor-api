a84aaf6b1f16de2851686a24ac087196d7bfe9c1
Evgenii Chernyshov <mailbox@eugesher.site>
2021-07-19 16:55:55 +0300

    feat: ingredient service

--- controllers/ingredients.js
+++ controllers/ingredients.js
@@ -1,51 +1,32 @@
-const Ingredient = require('../models/ingredient');
-const BadRequestError = require('../erorrs/bad-request-error');
-const { concatenateErrorMessages } = require('../utils');
+const IngredientService = require('../services/IngredientsService');
 
 module.exports.getIngredients = (req, res, next) => {
+  const service = new IngredientService(res, next);
   const { params } = req;
-  const { category } = params;
 
-  Ingredient.find(category && params)
-    .then((ingredients) => res.json(ingredients))
-    .catch(next);
+  service.getAll(params);
 };
 
 module.exports.addNewIngredient = (req, res, next) => {
+  const service = new IngredientService(res, next);
   const {
     name, category, quantity, price,
   } = req.body;
 
-  Ingredient.create({
-    name,
-    category,
-    quantity,
-    price,
-  })
-    .then((ingredient) => res.json(ingredient))
-    .catch((err) => next(
-      err.name === 'ValidationError' ? new BadRequestError(concatenateErrorMessages(err)) : err,
-    ));
+  service.create(name, category, quantity, price);
 };
 
 module.exports.removeIngredient = (req, res, next) => {
+  const service = new IngredientService(res, next);
   const { ingredientId } = req.params;
 
-  Ingredient.findByIdAndRemove(ingredientId)
-    .then((removedIngredient) => {
-      if (removedIngredient) res.json(removedIngredient);
-      else throw new BadRequestError('ingredient not found');
-    })
-    .catch((err) => next(err.kind === 'ObjectId' ? new BadRequestError('invalid ingredient id') : err));
+  service.remove(ingredientId);
 };
 
 module.exports.setIngredientsQuantity = (req, res, next) => {
+  const service = new IngredientService(res, next);
   const { ingredientId } = req.params;
   const { quantity } = req.body;
 
-  Ingredient.findByIdAndUpdate(ingredientId, { quantity }, { new: true })
-    .then((ingredient) => res.json(ingredient))
-    .catch((err) => next(
-      err.name === 'ValidationError' ? new BadRequestError(concatenateErrorMessages(err)) : err,
-    ));
+  service.setQuantity(ingredientId, quantity);
 };
---
+++ services/IngredientsService.js
@@ -0,0 +1,50 @@
+const Ingredient = require('../models/ingredient');
+const BadRequestError = require('../erorrs/bad-request-error');
+const { concatenateErrorMessages } = require('../utils');
+
+class IngredientsService {
+  constructor(res, next) {
+    this.res = res;
+    this.next = next;
+  }
+
+  getAll(params) {
+    const { category } = params;
+
+    Ingredient.find(category && params)
+      .then((ingredients) => this.res.json(ingredients))
+      .catch(this.next);
+  }
+
+  create(name, category, quantity, price) {
+    Ingredient.create({
+      name,
+      category,
+      quantity,
+      price,
+    })
+      .then((ingredient) => this.res.json(ingredient))
+      .catch((err) => this.next(
+        err.name === 'ValidationError' ? new BadRequestError(concatenateErrorMessages(err)) : err,
+      ));
+  }
+
+  remove(ingredientId) {
+    Ingredient.findByIdAndRemove(ingredientId)
+      .then((removedIngredient) => {
+        if (removedIngredient) this.res.json(removedIngredient);
+        else throw new BadRequestError('ingredient not found');
+      })
+      .catch((err) => this.next(err.kind === 'ObjectId' ? new BadRequestError('invalid ingredient id') : err));
+  }
+
+  setQuantity(ingredientId, quantity) {
+    Ingredient.findByIdAndUpdate(ingredientId, { quantity }, { new: true })
+      .then((ingredient) => this.res.json(ingredient))
+      .catch((err) => this.next(
+        err.name === 'ValidationError' ? new BadRequestError(concatenateErrorMessages(err)) : err,
+      ));
+  }
+}
+
+module.exports = IngredientsService;
