1c9046d624fe4f19837218eb76ef9f9cdf99cf85
Evgenii Chernyshov <mailbox@eugesher.site>
2021-07-20 16:37:38 +0300

    fix: throwing auth error

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(new UnauthorizedError('authorization required'));
+    throw new UnauthorizedError('authorization required');
   }
 
   const token = authorization.replace('Bearer ', '');
